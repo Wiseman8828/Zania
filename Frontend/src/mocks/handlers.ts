a57370ad0c595aa96f9f0499a88366ab06e90347
Ashwini <ashwini.kr.260@gmail.com>
2024-10-16 23:12:51 +0530

    if localStorage has no data load static data

--- Frontend/src/mocks/handlers.ts
+++ Frontend/src/mocks/handlers.ts
@@ -1,8 +1,12 @@
 import { rest } from 'msw'
-
+import {documents} from '../Data/documents.json'
 export const handlers = [
     rest.get('/api/documents', (req, res, ctx) => {
-        const storedData = JSON.parse(localStorage.getItem('documents') ?? '[]')
+        console.log(req)
+        let storedData = JSON.parse(localStorage.getItem('documents') ?? '[]')
+        if(storedData.length === 0) {
+            storedData = documents
+        }
         return res(ctx.status(200), ctx.json(storedData))
     }),
 
@@ -12,6 +16,7 @@ export const handlers = [
      */
     rest.post('/api/documents', (req:any, res, ctx) => {
         const data = req.body
+        console.log(data)
         const storedData = JSON.parse(localStorage.getItem('documents') ?? '[]')
         localStorage.setItem('documents', JSON.stringify(storedData))
         return res(
